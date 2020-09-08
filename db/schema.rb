e3cab43536aeae26afc55de23a83e587d33bc372
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-08 11:27:48 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_084030) do
+ActiveRecord::Schema.define(version: 2020_09_08_015417) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
