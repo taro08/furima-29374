7fa1507ca02ef43b0d95dda42a40f500127dd04d
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-10 19:09:12 +0900

    モデルの生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_015417) do
+ActiveRecord::Schema.define(version: 2020_09_10_091541) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category", null: false
+    t.integer "price", null: false
+    t.integer "status", null: false
+    t.integer "delivary_time", null: false
+    t.integer "consignor_area", null: false
+    t.integer "sipping_costs_burden", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_08_015417) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
