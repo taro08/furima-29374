459e6a72396736e136bc350100d2ba76f108b67a
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-11 09:18:02 +0900

    Active_Storageの導入【ストロングパラメーター】

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
