ad150e0e8f127fbeef51fedc6ac3959bff73ea2c
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-12 17:59:22 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
   resources :items, only: [:index, :new, :create]
+  resources :orders, only: [:index]
+  resources :delivery, only: [:index]
 end
