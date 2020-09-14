69627123170c634e6782e024e96b0fc6c98943e6
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-14 10:55:07 +0900

    ビューの実装

--- db/migrate/20200910091541_create_items.rb
+++ db/migrate/20200910091541_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,                  null: false
-      t.text :description,             null: false
-      t.integer :category,             null: false
-      t.integer :price,                null: false
-      t.integer :status,               null: false
-      t.integer :delivery_time,        null: false
-      t.integer :consignor_area,       null: false
-      t.integer :shipping_costs_burden, null: false
-      t.references :user,              null: false, foreign_key: true
+      t.string :name,                      null: false
+      t.text :description,                 null: false
+      t.integer :category_id,              null: false
+      t.integer :price,                    null: false
+      t.integer :status_id,                null: false
+      t.integer :delivery_time_id,         null: false
+      t.integer :consignor_area_id,        null: false
+      t.integer :shipping_costs_burden_id, null: false
+      t.references :user,                  null: false, foreign_key: true
       t.timestamps
     end
   end
