12f4f1dd92c559516817c17595432eae1154689e
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-18 18:40:35 +0900

    バリデーションの実装

--- app/forms/order_delivery.rb
+++ app/forms/order_delivery.rb
@@ -2,6 +2,15 @@ class OrderDelivery
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :order_id, :token
 
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id,format: { other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :house_number
+    validates :phone_number,format: {with: /\A\d{10}$|^\d{11}\z/}
+  end
+
+
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     binding.pry
