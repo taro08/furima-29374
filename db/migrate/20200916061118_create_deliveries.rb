5dc9dae23c96bb1f34fffcc1b8a0190bed4b8541
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-18 15:39:16 +0900

    prefectureのカラム名の修正

--- db/migrate/20200916061118_create_deliveries.rb
+++ db/migrate/20200916061118_create_deliveries.rb
@@ -2,7 +2,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
       t.string :postal_code, null: false
-      t.integer :prefecture, null: false
+      t.integer :prefecture_id, null: false
       t.string :city, null: false
       t.string :house_number, null: false
       t.string :building
