d9fdfcd75962ac5275a507072f7ef46fd747a2c1
taro08 <69657352+taro08@users.noreply.github.com>
2020-09-18 16:28:49 +0900

    カードの実装＋Controller＋モデルの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,37 @@ class OrdersController < ApplicationController
     @order = Order.new
     @item = Item.find(params[:item_id])
   end
+
+  def new
+    @order = OrderDelivery.new()
+  end
+
   def create
+
+    @order = OrderDelivery.new(order_params)
+    @item = Item.find(params[:item_id])
+    binding.pry
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    end
+    render 'index'
+  end
+
+  private
+
+  def order_params
+    params.permit(:token, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number,:order_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency:'jpy'
+    )
 
   end
 end
---
+++ app/forms/order_delivery.rb
@@ -0,0 +1,10 @@
+class OrderDelivery
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :order_id, :token
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    binding.pry
+    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end 
