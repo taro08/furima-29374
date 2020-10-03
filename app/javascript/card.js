8225b239d22b4a79a3de7c912f980c7d6d490951
taro08 <69657352+taro08@users.noreply.github.com>
2020-10-03 14:25:55 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,13 +18,13 @@ const pay = () => {
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='card_token'>`;
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
  
-        document.getElementById("number").removeAttribute("name");
-        document.getElementById("cvc").removeAttribute("name");
-        document.getElementById("exp_month").removeAttribute("name");
-        document.getElementById("exp_year").removeAttribute("name");
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
  
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
